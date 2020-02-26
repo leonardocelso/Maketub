bc37619fb66ddd506efa35412fcb908cf6b81b12
Danilo Fogaça <daniloapfogaca@gmail.com>
2020-02-26 11:13:35 -0300

    Primeiro up

---
+++ Maktub.Portal.Web/Controllers/ControleAcessoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Maktub.Portal.Web.Controllers
+{
+    public class ControleAcessoController : Controller
+    {
+        public IActionResult Usuario()
+        {
+            return View();
+        }
+    }
+}
